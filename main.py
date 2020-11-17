b2ddfb4f76f5207d943c231ec1b8a7304d93c07b
Kletcka <56651299+kletcka@users.noreply.github.com>
2020-11-17 18:27:56 +0300

    Add files via upload

--- main.py
+++ main.py
@@ -1,32 +1,70 @@
 import random
 import objects
+
+import json
 import pygame
 import time
 pygame.init()
 
-WIDTH = 600
-HEIGHT = 600
+settings = dict()
+
+with open('settings.json', 'r', encoding='utf-8') as read_file:
+    settings = json.load(read_file)
+
+HM = int(settings['set_numbers_of_cells'])
+WI =  int(settings['set_width_of_cells'])
+WIDTH = HM*WI
+HEIGHT = HM*WI
+BACKFILL = (settings['colors']['back_color']['r'], settings['colors']['back_color']['g'], settings['colors']['back_color']['b'])
+TEXTFILL = (settings['colors']['text_color']['r'], settings['colors']['text_color']['g'], settings['colors']['text_color']['b'])
+NEXTN = settings['texts']['new']
+ANSWERS = {True: settings['texts']['x_wins'], False:  settings['texts']['o_wins'], None:  settings['texts']['no_wins']}
+
 
 screen = pygame.display.set_mode((WIDTH, HEIGHT))
 clock = pygame.time.Clock()
-hm = 5
-list_of_f = {}
-alli = ['lu', 'ld', 'ru', 'rd', 'u', 'd', 'l', 'r']
-liste = {'lu': ('l', 1), 'ld': ('l', 0), 'ru': ('r', 1), 'rd': (
-    'r', 0), 'u': ('v', 1), 'd': ('v', 0), 'l': ('g', 1), 'r': ('g', 0), }
-for i in range(0, hm):
-    for j in range(0, hm):
-        list_of_f[i, j] = objects.Field(i*WIDTH//hm, j*HEIGHT//hm)
+
+
+
+liste = {'lu': ('l', 2),
+    'ld': ('l', 1),
+    'ru': ('r', 2),
+    'rd': ('r', 1),
+    'u': ('v', 2),
+    'd': ('v', 1),
+    'l': ('g', 2),
+    'r': ('g', 1),
+
+    'lu1': ('l', 3),
+    'ld1': ('l', 0),
+    'ru1': ('r', 3),
+    'rd1': ('r', 0),
+    'u1': ('v', 3),
+    'd1': ('v', 0),
+    'l1': ('g', 3),
+    'r1': ('g', 0),
+        }
+
+
 
 done = False
 which = True
-answers = {True: 'Крестики выиграли', False: 'Нолики выиграли', None: 'Ничья'}
 ans = None
 mode = True
+list_of_f = {}
 
+
+def reset():
+    global list_of_f
+    list_of_f = {}
+    for i in range(0, HM):
+        for j in range(0, HM):
+            list_of_f[i, j] = objects.Field(i*WI, j*WI, WI)
+
+reset()
 
 while not done:
-    screen.fill((0, 0, 0))
+    screen.fill(BACKFILL)
     clock.tick(60)
     if mode == True:
         for i in pygame.event.get():
@@ -37,7 +75,7 @@ while not done:
                 for i in list_of_f:
                     if list_of_f[i].rect.collidepoint(pos) and list_of_f[i].stage == None:
                         list_of_f[i].change(which)
-                        pos = (list_of_f[i].x//(WIDTH//hm), list_of_f[i].y//(HEIGHT//hm))
+                        pos = (list_of_f[i].x//(WIDTH//HM), list_of_f[i].y//(HEIGHT//HM))
                         l = {}
                         l['lu'] = (pos[0]-1, pos[1]-1)
                         l['ld'] = (pos[0]+1, pos[1]+1)
@@ -48,7 +86,15 @@ while not done:
                         l['l'] = (pos[0]-1, pos[1])
                         l['r'] = (pos[0]+1, pos[1])
 
-                        for i in alli:
+                        l['lu1'] = (pos[0]-2, pos[1]-2)
+                        l['ld1'] = (pos[0]+2, pos[1]+2)
+                        l['ru1'] = (pos[0]+2, pos[1]-2)
+                        l['rd1'] = (pos[0]-2, pos[1]+2)
+                        l['u1'] = (pos[0], pos[1]-2)
+                        l['d1'] = (pos[0], pos[1]+2)
+                        l['l1'] = (pos[0]-2, pos[1])
+                        l['r1'] = (pos[0]+2, pos[1])
+                        for i in list(liste.keys()):
                             a = l[i]
                             b = liste[i]
                             try:
@@ -59,7 +105,7 @@ while not done:
                         which = not which
             elif pygame.mouse.get_pressed()[0] == 1 and pygame.key.get_pressed()[pygame.K_DOWN]:
                 pos = pygame.mouse.get_pos()
-                d = list_of_f[pos[0]//(WIDTH//hm), pos[1]//(HEIGHT//hm)]
+                d = list_of_f[pos[0]//(WIDTH//HM), pos[1]//(HEIGHT//HM)]
                 print(d.hi)
                 for f in ['g', 'v', 'l', 'r']:
                     if dict(d.hi)[f] == [True, True] and d.stage == True:
@@ -90,20 +136,18 @@ while not done:
             if pygame.key.get_pressed()[pygame.K_SPACE]:
                 mode = True
                 list_of_f = {} 
-                for f in range(0, hm):
-                    for j in range(0, hm):
-                        list_of_f[f, j] = objects.Field(f*WIDTH//hm, j*HEIGHT//hm)
+                reset()
                 which = True
                 ans = None
-        font = pygame.font.Font(pygame.font.match_font('arial'), 70)
-        text_surface = font.render(answers[ans], True, (255, 255, 255))
+        font = pygame.font.Font(pygame.font.match_font('arial'), WIDTH//10)
+        text_surface = font.render(ANSWERS[ans], True, TEXTFILL)
         text_rect = text_surface.get_rect()
-        text_rect.midtop = (300, 100)
+        text_rect.midtop = (WIDTH//2, HEIGHT//3)
         screen.blit(text_surface, text_rect)
-        font = pygame.font.Font(pygame.font.match_font('arial'), 35)
-        text_surface1 = font.render('Нажмите на пробел, чтобы продолжить', True, (255, 255, 255))
+        font = pygame.font.Font(pygame.font.match_font('arial'),  WIDTH//20)
+        text_surface1 = font.render(NEXTN, True, TEXTFILL)
         text_rect1 = text_surface.get_rect()
-        text_rect1.midtop = (290, 400)
+        text_rect1.midtop = (WIDTH//2, HEIGHT//3*2)
         screen.blit(text_surface, text_rect)
         screen.blit(text_surface1, text_rect1)
     pygame.display.flip()
--- objects.py
+++ objects.py
@@ -1,34 +1,35 @@
 import pygame
 import os
 class Field(pygame.sprite.Sprite):
-    def __init__(self, x, y):
+    def __init__(self, x, y, scale):
         self.x = x
         self.y = y
+        self.scale = scale
         self.image_adress = os.path.join('Images', 'None.png')
         self.my_image = pygame.image.load(self.image_adress).convert_alpha()
-        self.my_image = pygame.transform.scale(self.my_image, (120, 120))
+        self.my_image = pygame.transform.scale(self.my_image, (self.scale , self.scale ))
         self.stage = None
-        self.rect = pygame.Rect(self.x, self.y, 120, 120)
+        self.rect = pygame.Rect(self.x, self.y, self.scale , self.scale )
         self.hi = {
-            'g':[None, None],
-            'v':[None, None],
-            'r':[None, None],
-            'l':[None, None],
+            'g':[None]*4,
+            'v':[None]*4,
+            'r':[None]*4,
+            'l':[None]*4,
         }
     def change(self, how):
         self.stage = how
         self.image_adress = os.path.join('Images', f'{str(how)}.png')
         self.my_image = pygame.image.load(self.image_adress).convert_alpha()
-        self.my_image = pygame.transform.scale(self.my_image, (120, 120))
+        self.my_image = pygame.transform.scale(self.my_image, (self.scale , self.scale ))
     def crew(self, how, vect, ind):
         self.hi[vect][ind] = how
     def check(self):
         ret = None
         for i in ['g', 'v', 'l', 'r']:
-                if dict(self.hi)[i] == [True, True] and self.stage == True:
+                if dict(self.hi)[i] == [True]*4 and self.stage == True:
                     ret = True
                     break
-                elif  dict(self.hi)[i] == [False, False] and self.stage == False:
+                elif  dict(self.hi)[i] == [False, False, True, True] and self.stage == False:
                     ret = False
                     break
                 else:
